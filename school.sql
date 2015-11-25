af782f446afeabd6680c9bb19903048bb9d8f70d
kfrank <kassandrafrank@web.de>
2015-11-25 20:47:33 +0100

    School-SQL-Skript
    SQL-Skript um die Datenbank school mit Tabellen zu erzeugen

--- school.sql
+++ school.sql
@@ -1,7 +1,12 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Schema school
 -- -----------------------------------------------------
@@ -77,3 +82,44 @@ DEFAULT CHARACTER SET = utf8;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `school`.`address`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `school`;
+INSERT INTO `school`.`address` (`ad_ID`, `ad_street`, `ad_number`, `ad_city`, `ad_postcode`) VALUES (1, 'Hauptstraße', '7', 'Karlsruhe', 76133);
+INSERT INTO `school`.`address` (`ad_ID`, `ad_street`, `ad_number`, `ad_city`, `ad_postcode`) VALUES (2, 'Seestraße', '15', 'Karlsruhe', 76131);
+INSERT INTO `school`.`address` (`ad_ID`, `ad_street`, `ad_number`, `ad_city`, `ad_postcode`) VALUES (3, 'Goethestraße', '67', 'Karlsruhe', 76133);
+INSERT INTO `school`.`address` (`ad_ID`, `ad_street`, `ad_number`, `ad_city`, `ad_postcode`) VALUES (4, 'Hauptstraße', '121', 'Karlsruhe', 76133);
+INSERT INTO `school`.`address` (`ad_ID`, `ad_street`, `ad_number`, `ad_city`, `ad_postcode`) VALUES (5, 'Janstraße', '2', 'Karlsruhe', 76132);
+INSERT INTO `school`.`address` (`ad_ID`, `ad_street`, `ad_number`, `ad_city`, `ad_postcode`) VALUES (6, 'Bruchsaler Weg', '21', 'Karlsruhe', 76131);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `school`.`pupil`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `school`;
+INSERT INTO `school`.`pupil` (`pu_ID`, `pu_christianname`, `pu_name`, `pu_address_ID`) VALUES (1, 'Anne', 'Schmidt', 2);
+INSERT INTO `school`.`pupil` (`pu_ID`, `pu_christianname`, `pu_name`, `pu_address_ID`) VALUES (2, 'Niclas', 'Müller', 1);
+INSERT INTO `school`.`pupil` (`pu_ID`, `pu_christianname`, `pu_name`, `pu_address_ID`) VALUES (3, 'Andreas', 'Schuster', 3);
+INSERT INTO `school`.`pupil` (`pu_ID`, `pu_christianname`, `pu_name`, `pu_address_ID`) VALUES (4, 'Nina', 'Perl', 6);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `school`.`teacher`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `school`;
+INSERT INTO `school`.`teacher` (`te_ID`, `te_christianname`, `te_name`, `te_address_ID`) VALUES (1, 'Michael', 'Müller', 1);
+INSERT INTO `school`.`teacher` (`te_ID`, `te_christianname`, `te_name`, `te_address_ID`) VALUES (2, 'Nicole', 'Wagner', 4);
+INSERT INTO `school`.`teacher` (`te_ID`, `te_christianname`, `te_name`, `te_address_ID`) VALUES (3, 'Jasmin', 'Becker', 7);
+INSERT INTO `school`.`teacher` (`te_ID`, `te_christianname`, `te_name`, `te_address_ID`) VALUES (4, 'Thomas', 'Schmitt', 5);
+
+COMMIT;
+
